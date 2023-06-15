411b1b6139dde8861c25b065cec1800f9f81863f
eric <eric.tondelli@vision-e.it>
2023-06-15 17:40:56 +0200

    add cmake deps to opencv

--- example/Cpp_native/main.cpp
+++ example/Cpp_native/main.cpp
@@ -5,20 +5,21 @@
 #include <imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
 
+#include <opencv2/opencv.hpp>
 #include <cstdio>
 
 int main()
 {
     // Initialize GLFW
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui Example", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "ImGui Example", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
     // Initialize ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     (void)io;
 
     ImGui::StyleColorsDark();
@@ -27,6 +28,9 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
+    cv::Mat cvImage;
+    cvImage= cv::imread("../dog.jpg");
+    
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
@@ -47,6 +51,28 @@ int main()
             // (For this example, we will just print a message)
             printf("Button is clicked!\n");
         }
+
+        cv::Mat cvImageRGBA;
+        cv::cvtColor(cvImage, cvImageRGBA, cv::COLOR_BGR2RGBA);
+        const unsigned char *imagePixels = cvImageRGBA.ptr<unsigned char>();
+
+        // Get the image dimensions
+        int imageWidth = cvImage.cols;
+        int imageHeight = cvImage.rows;
+
+        // Create an ImGui texture from the image data
+        ImTextureID textureID = ImGui::GetIO().Fonts->TexID;
+        ImGui::GetIO().Fonts->TexID = (void *)(intptr_t)textureID;
+        textureID = ImGui::GetIO().Fonts->TexID;
+        ImGui::GetIO().Fonts->TexID = (void *)(intptr_t)textureID;
+        ImGui::GetIO().Fonts->TexID = (void *)(intptr_t)textureID;
+        ImGui::GetIO().Fonts->TexID = (void *)(intptr_t)textureID;
+
+        // Display the image using ImGui
+        ImGui::Image(textureID, ImVec2(imageWidth, imageHeight));
+
+        // Restore the original ImGui texture ID
+        ImGui::GetIO().Fonts->TexID = textureID;
 
         ImGui::End();
 
