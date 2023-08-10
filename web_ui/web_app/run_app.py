f98c5d6c492969b2e679d60158f4019de3cc2f12
eric_windows <eric.tondelli@gmail.com>
2023-08-10 10:49:57 +0200

    remove cfg from git repository

--- web_ui/web_app/run_app.py
+++ web_ui/web_app/run_app.py
@@ -1,23 +1,42 @@
-from app import create_app,create_db_path
-
-# NOTE: Explicitly import configuration so that PyInstaller is able to find and bundle it
 import config
 import yaml
 import os
+from app import create_app, create_db_path
 
-#Set current file path to load config independent from python launch path
-current_exec_path= os.path.abspath(__file__)
-root_exec=os.path.dirname(current_exec_path)
-yaml_cfg= root = root_exec+"/gui_cfg.yaml"
+#DEFAULT CFG
 
-db_uri=None
-#Read configuration and set db Path before app initialization
+conf_dict_default = {
+    'experiment_name': 'testWindows',
+    'base_path_experiment': 'C:/Users/erict/OneDrive/Desktop/Develop/Experiment/classificaton_gui/',
+    'task':"classification",
+    'dataset_path': 'C:/Users/erict/OneDrive/Desktop/Develop/Dataset/DVintel/Data/Dataset/',
+    'dataset_versioning': False,
+    'data_version_tag': 'v4',
+    'data_version_name': 'Dataset.dvc',
+}
+
+db_uri = None
+# Set current file path to load config independent from python launch path
+current_exec_path = os.path.abspath(__file__)
+root_exec = os.path.dirname(current_exec_path)
+yaml_cfg = root = root_exec+"/gui_cfg.yaml"
+
+if not os.path.exists(yaml_cfg):
+    # Create the YAML file and write the initial dictionary
+    with open(yaml_cfg, 'w') as yaml_file:
+        yaml.dump(conf_dict_default, yaml_file, default_flow_style=False)
+    print(f"Created {yaml_cfg} and initialized with initial dictionary.")
+else:
+    print(f"{yaml_cfg} already exists. Skipping creation.")
+
+
+# Read configuration and set db Path before app initialization
 with open(yaml_cfg, "r") as yaml_file:
     main_cfg = yaml.safe_load(yaml_file)
 
-    db_uri= create_db_path(main_cfg,root_exec)
+    db_uri = create_db_path(main_cfg, root_exec)
 
-application = create_app("config.DevelopmentConfig",db_uri)
+application = create_app("config.DevelopmentConfig", db_uri)
 
 import app.turbo_wrapper.turbo_wrapper_call
 
@@ -28,5 +47,4 @@ https://stackoverflow.com/a/63892938
 """
 if __name__ == "__main__":
 
-    application.run(host="127.0.0.1", port=4040, use_reloader=True,debug=True)
-    
+    application.run(host="127.0.0.1", port=4040, use_reloader=True, debug=True)
