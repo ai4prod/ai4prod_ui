02c67f097e743ef0e6f03b90a7093147f04b0eaf
Eric Tondelli <eric.tondelli@gmail.com>
2023-09-04 11:44:18 +0200

    add script to release a new image

---
+++ web_ui/docker/script/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the container name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <container_name>"
+    exit 1
+fi
+
+# Get the container name from the command line argument
+container_name="docker_ai4prod_ui_container_dev_1"
+
+# Use `docker exec` to start an interactive shell in the container
+echo "Write exit and then press Enter To Create a new image ai4prod/ai4prod:base"
+docker exec -it "$container_name" /bin/bash
+ssh-add -D
+
+docker container stop "$container_name"
+docker commit "$container_name" ai4prod/ai4prod_ui:base
+docker start "$container_name"
